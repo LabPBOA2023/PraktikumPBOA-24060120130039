3e90ebe1b6e12948da38aae3cec00aba294e809a
Hikmal Arkan N.Z <hikmalarkannurzaidan@gmail.com>
2023-03-01 15:32:20 +0700

    Create Titik.java

---
+++ Pertemuan 1/Titik.java
@@ -0,0 +1,46 @@
+/**
+ * File		: MOperasiTitik.java	01/03/23
+ * Penulis		: Hikmal Arkan Nur Zaidan
+ * Deskripsi	: kelas yang berisi program utama yang memanfaatkan 
+ *			   kelas MOperasiTitik
+ * 
+ */ 
+ 
+ class Titik{
+	double absis; 
+	double ordinat; 
+	static int counterTitik;
+  
+	Titik(double a,double o){
+		counterTitik++;
+		absis = a;
+		ordinat = o;
+	}
+  
+	Titik(){
+		counterTitik++;
+		absis = 0;
+		ordinat = 0;
+	}
+		
+	void setAbsis(double a){
+		absis = a;
+	}
+	
+	void setOrdinat(double o){
+		ordinat = o;
+	}
+	
+	double getAbsis(){
+		return absis;
+	}
+	
+	double getOrdinat(){
+		return ordinat;
+	}
+	
+	int getCounterTitik(){
+		return counterTitik;
+	}
+
+ }
